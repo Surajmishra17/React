f0b1fe584c1bf54516bc6e70526418181c3299b6
Suraj Mishra <surajmishra88266@gmail.com>
2026-01-04 16:54:20 +0530

    Basics

---
+++ 01basicreact/src/App.jsx
@@ -0,0 +1,13 @@
+import Temp from "./temp"
+function App() {
+
+  return (
+    // <Temp></Temp>
+    <>
+      <Temp />
+      <h1>React learning</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 01basicreact/src/temp.jsx
@@ -0,0 +1,7 @@
+function Temp(){
+    return(
+        <h3>This is temp file</h3>
+    )
+}
+
+export default Temp
