baef6e6cb3b7ef6b4ccad8e2ec09556a0e52be53
Suraj Mishra <surajmishra88266@gmail.com>
2026-01-05 11:50:06 +0530

    Custom-react

--- 01basicreact/src/App.jsx
+++ 01basicreact/src/App.jsx
@@ -1,13 +1,15 @@
 import Temp from "./temp"
 function App() {
+  const username = "John"
 
   return (
     // <Temp></Temp>
     <>
       <Temp />
-      <h1>React learning</h1>
+      <h1>React learning {username}</h1> 
     </>
   )
 }
-
+ 
+//{}->evaluated expression it is final outcome of javascript 
 export default App
--- 01basicreact/src/main.jsx
+++ 01basicreact/src/main.jsx
@@ -1,9 +1,39 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <div>
+      <h1>custom app</h1>
+    </div>
+  )
+}
+
+// const ReactElement = {
+//   type: 'a',
+//   props: {
+//     href: 'https://google.com',
+//     target: '_blank'
+//   },
+//   children: 'Click me to visit google'
+// }
+
+const anotherUser = "Don"
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+const ReactElement = React.createElement(
+  'a',
+  {
+    href: 'https://google.com',
+  },
+  'click me to visit google',
+  anotherUser
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    ReactElement
 )
